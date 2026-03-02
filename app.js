f4fc31ab8743a25730b39993af514833b71b274a
frankely29 <71986544+frankely29@users.noreply.github.com>
2026-03-02 12:26:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1008,7 +1008,7 @@ function startLocationWatch() {
 
       if (!gpsFirstFixDone) {
         gpsFirstFixDone = true;
-        const targetZoom = Math.max(map.getZoom(), 12.5);
+        const targetZoom = Math.max(map.getZoom(), 12);
         suppressAutoDisableFor(1200, () => map.setView(userLatLng, targetZoom, { animate: true }));
       } else {
         if (autoCenter) {
