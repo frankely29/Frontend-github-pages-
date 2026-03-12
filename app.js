a6f469a42961f9d47005d8c4a9d7ca29dad23dc5
frankely29 <71986544+frankely29@users.noreply.github.com>
2026-03-12 19:26:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1588,7 +1588,7 @@ async function ensureZonesSourceAndLayers() {
           10, 00,
           11, 06,
           12, 11,
-          13, 17,
+          13, 18,
         ],
         "text-max-width": ["coalesce", ["get", "textMaxWidth"], 4],
         "text-letter-spacing": ["coalesce", ["get", "letterSpacing"], 0],
